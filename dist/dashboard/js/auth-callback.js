5354e817ae40f5fd98984e3d22c5c1afaabc2a33
Terry Ott <Terry.Ott@publicntp.org>
2023-07-31 22:44:01 +0000

    Store cookies, clean up URL

--- dist/dashboard/js/auth-callback.js
+++ dist/dashboard/js/auth-callback.js
@@ -1,17 +1,23 @@
 function processAuthTokens( accessToken, refreshToken, accessTokenExpiration ) {
+    /*
     console.log( "           Access token : " + accessToken );
     console.log( "          Refresh token : " + refreshToken );
     console.log( "Access token expiration : " + accessTokenExpiration );
-
-    // Set the cookie 
-    /*
-    document.cookie = "FGA_ACCESS_TOKEN=" + accessToken;
-
-    console.log( "FGA_ACCESS_TOKEN cookie set" );
     */
+
+    // Set browser cookies
+    document.cookie = "LETSVAL_ACCESS_TOKEN=" + accessToken;
+    document.cookie = "LETSVAL_REFRESH_TOKEN=" + refreshToken;
+    document.cookie = "LETSVAL_TOKEN_EXPIRATION=" + accessTokenExpiration;
+
+    console.log( "Auth cookies set" );
 }
 
 function cleanUpUrl() {
+    // Overwrite the URL to remove all the code stuff
+    history.replaceState(null, null, "https://letsvaliate.org/dashboard" );
+
+    console.log( "URL rewritten to remove tokens now that they are stored in cookies" );
 }
 
 
@@ -23,7 +29,8 @@ const hasRefreshToken   = urlParams.has( "refresh_token" );
 const hasExpirationDate = urlParams.has( "access_token_expiration" );
 
 if ( hasAccessToken && hasRefreshToken && hasExpirationDate ) {
-    processAuthTokens( urlParams.get("access_token"),
+    processAuthTokens( 
+        urlParams.get("access_token"),
         urlParams.get("refresh_token"), 
         urlParams.get("access_token_expiration") );
     cleanUpUrl();
