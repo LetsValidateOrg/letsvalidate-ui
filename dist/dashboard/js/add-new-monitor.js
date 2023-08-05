453581d58b25fa272b457a3e03afefdfea1f3729
Terry Ott <Terry.Ott@publicntp.org>
2023-08-05 21:30:45 +0000

    Testing button enable disable logic

--- dist/dashboard/js/add-new-monitor.js
+++ dist/dashboard/js/add-new-monitor.js
@@ -1,9 +1,31 @@
 function addNewMonitorUrl() {
     console.log("Button clicked to add new URL to monitor");
 }
+
+function checkAddNewUrlInputActions() {
+    // Check for events that affect the submit button
+    const newUrlText = document.getElementById("input_new_monitor_url").value;
+
+    const isButtonDisabled = document.getElementById("button_add_new_monitor").disabled;
+
+    const newTextIsValidUrl = ((newUrlText.length > 5) && (newUrlText.includes(".")));
+
+    // if the button is currently disabled, see if we can enable it
+    if ( (isButtonDisabled === true) && (newTextIsValidUrl === true) ) {
+        document.getElementById("button_add_new_monitor").disabled = false;
+    }
+
+    // if the button is currently enabled, let's see if we should disable it
+    else if ( (isButtonDisabled === false) && (newTextIsValidUrl === false) ) {
+        document.getElementById("button_add_new_monitor").disabled = true; 
+    }
+}
+
 function addEventListeners() {
     document.getElementById("button_add_new_monitor").addEventListener(
         "click", addNewMonitorUrl );
+    document.getElementById("input_new_monitor_url").addEventListener(
+        "input", checkAddNewUrlInputActions );
 }
 
 addEventListeners();
