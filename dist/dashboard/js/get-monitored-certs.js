981aca4d79508058256880f2fd4e276071350eef
Terry Ott <Terry.Ott@publicntp.org>
2023-08-27 14:50:06 +0000

    Seeing if we got actual worker KV data

--- dist/dashboard/js/get-monitored-certs.js
+++ dist/dashboard/js/get-monitored-certs.js
@@ -20,6 +20,8 @@ async function requestMonitoredCerts() {
     console.log("Got response to request for monitored certs in " + fetchTimeInMs + " ms");
     if ( fetchResponse.ok ) {
         console.log("Got 200-something response from monitored certs request");
+        const jsonBody = await fetchResponse.json();
+        console.log( JSON.stringify(jsonBody, null, 2) );
     } else {
         console.log("Got non-200 response for monitored certs");
     }
