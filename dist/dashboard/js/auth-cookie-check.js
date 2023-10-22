eb050edacad39299b70a35dbba206523d09b20a3
Terry Ott <Terry.Ott@publicntp.org>
2023-10-22 20:58:15 +0000

    Setting URL for login based on login URL

--- dist/dashboard/js/auth-cookie-check.js
+++ dist/dashboard/js/auth-cookie-check.js
@@ -1,4 +1,12 @@
-const cognitoHostedLoginUrl = "https://letsvalidate.auth.us-east-2.amazoncognito.com/oauth2/authorize?client_id=rme10ok7gdr32r7qgoei8tocv&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwvyfbi1fnf.execute-api.us-east-2.amazonaws.com%2Foauth%2Fcallback";
+const hostedLoginUrls = {
+    letsvalidate.org                    : "https://letsvalidate.auth.us-east-2.amazoncognito.com/oauth2/authorize?client_id=sjra3i7iq5ptenkg379vsar2&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwvyfbi1fnf.execute-api.us-east-2.amazonaws.com%2Foauth%2Fcallback%2Fprod",
+    staging.letsvalidate-ui.pages.dev   : "https://letsvalidate.auth.us-east-2.amazoncognito.com/oauth2/authorize?client_id=78ich2vt28i9p89jf87mgg3qdq&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwvyfbi1fnf.execute-api.us-east-2.amazonaws.com%2Foauth%2Fcallback%2Fstaging",
+    dev-cody.letsvalidate-ui.pages.dev  : "https://letsvalidate.auth.us-east-2.amazoncognito.com/oauth2/authorize?client_id=hgd8grnghg8vtrmbj9u9gaf3l&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwvyfbi1fnf.execute-api.us-east-2.amazonaws.com%2Foauth%2Fcallback%2Fdev-cody",
+    dev-eric.letsvalidate-ui.pages.dev  : "https://letsvalidate.auth.us-east-2.amazoncognito.com/oauth2/authorize?client_id=1aq3d5q40ce3lhdijghi2pi1gn&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwvyfbi1fnf.execute-api.us-east-2.amazonaws.com%2Foauth%2Fcallback%2Fdev-eric",
+    dev-terry.letsvalidate-ui.pages.dev : "https://letsvalidate.auth.us-east-2.amazoncognito.com/oauth2/authorize?client_id=4qto2qut19rglo6h6aq7qi98ro&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwvyfbi1fnf.execute-api.us-east-2.amazonaws.com%2Foauth%2Fcallback%2Fdev-terry"
+};
+
+const cognitoHostedLoginUrl = hostedLoginUrls[window.location.hostname];
 
 function getCookie( cookieName ) {
   const name = cookieName + "=";
