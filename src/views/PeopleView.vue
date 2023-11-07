4e5c2bc5d0a1e8964997f4df1f1acf4c54ec710e
Eric Evans <zipbug@erics-mbp.lan>
2023-11-07 13:55:34 -0700

    added some stolen content from PNTP

--- src/views/PeopleView.vue
+++ src/views/PeopleView.vue
@@ -2,22 +2,36 @@
   <main class="people container">
     <div class="row">
       <div class="col">
-          <h1>People</h1>
+        <h1>People</h1>
       </div>
     </div>
-    <div class="row">
+    <div class="row mt-4">
       <div class="col-3">
         <img src="../assets/terry.jpg" />
       </div>
       <div class="col-9">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. </p>
-        <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. </p>
+        <h2>Terry Ott</h2>
+        <p>
+          Growing up in Denver, Colorado, I'd often ask my parents if they'd let
+          me call the NIST Telephone Time-of-Day Service in the nearby city of
+          Boulder to hear the current time according to the atomic clocks.
+        </p>
+        <p>
+          During my college years, friends rolled their eyes when they saw me
+          calibrating my wristwatch with the atomic time on the University
+          central servers. As I became deeply interested in Linux system
+          adminstration, it was a near-certainty to find an NTP daemon running
+          on any host I had root privs on.I founded PublicNTP in 2008 based on a
+          deeply-held belief that highly-accurate time data should be provided
+          as an unrestricted public service, open to all systems on the
+          Internet.
+        </p>
+
+        <p>Location: Northern Virginia (Washington DC metro area)</p>
+        <p>Occupation: Software Engineer III</p>
+        <p>Favorite superhero: Wolverine. One word: adamantium!</p>
       </div>
     </div>
-    <br/>
-    <div class="row">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. </p>
-      <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. </p>
-    </div>
+    <br />
   </main>
 </template>
--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -1,15 +1,234 @@
 <template>
-    <div class="terms container">
-        <div class="row">
-            <div class="col">
-                <h1>Terms of Use</h1>
-                <br/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. </p>
-                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. </p>
-                <p>Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. </p>
-                <p>Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. </p>
-                <p>Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. </p>
-            </div>
-        </div>
+  <main class="terms container">
+    <div class="row">
+      <div class="col">
+        <h1>Terms of Use</h1>
+        <br />
+        <p><b>ARTICLE 1 DEFINITIONS</b></p>
+        <p>
+          Let's Validate, Inc. (“Let's Validate”) is a Non-Profit entity with a
+          principal place of business located at:
+        </p>
+
+        <p>1405 S Fern St #90372</p>
+        <p>Arlington VA 22202-2810</p>
+
+        <p>
+          Let's Validate has created a monitoring service (“SERVICE”) that is
+          the source of these Terms of Service.
+        </p>
+
+        <p>
+          By using this SERVICE, YOU agree to the following terms and conditions
+          of use.
+        </p>
+
+        <p><b>ARTICLE 2 GRANT OF LICENSE</b></p>
+        <p>
+          This SERVICE, if legally executed as defined herein, licenses and so
+          grants YOU the following rights:
+        </p>
+
+        <p>SERVICE: YOU may use this SERVICE an unlimited number of times.</p>
+
+        <p>
+          YOU agree to use the SERVICE only for purposes that are permitted by
+          (a) the Terms and Conditions and (b) any applicable law, regulation or
+          generally accepted practices or guidelines in the relevant
+          jurisdictions (including, but not limited to, any laws regarding
+          Consumer Privacy, Intellectual Property and the export of data or
+          SERVICE to and from the United States or other relevant countries).
+        </p>
+
+        <p><b>ARTICLE 3 DELIVERY</b></p>
+        <p>
+          The SERVICE shall be made available to YOU electronically. By
+          accepting its use upon the first time, YOU freely acknowledge and
+          accept these Terms and Conditions.
+        </p>
+        <p><b>ARTICLE 4 RESTRICTIONS</b></p>
+        <p>
+          YOU acknowledge and agree that the title to and ownership of the
+          SERVICE and the intellectual property rights associated therewith are
+          and remain the sole and exclusive property of Let's Validate and
+          agents and/or licensors. Because Let's Validate’s mission is to expand
+          NTP servers around the world, Let's Validate grants YOU a revocable
+          license to allow modification of any open source applications and
+          inclusion of the SERVICE in YOUR application.
+        </p>
+        <p><b>ARTICLE 5 DISCLAIMER OF WARRANTIES</b></p>
+        <p>
+          YOU expressly acknowledge and agree that use of the SERVICE is at YOUR
+          sole risk.
+        </p>
+        <p>
+          The SERVICE, and source code are provided "AS IS”, and on a best-
+          effort basis, without warranty of any kind. Let's Validate does not,
+          either expressed, implied or statutory, make any warranties, claims or
+          representations with respect to the SERVICE, including, without
+          limitation, warranties of quality, performance, merchantability, or
+          fitness of use for a particular purpose, to the fullest extent
+          allowable by law, unless explicitly stated otherwise in these Terms
+          and Conditions.
+        </p>
+        <p>
+          YOU acknowledge that the quality of SERVICE may be affected by factors
+          beyond Let's Validate’s control, including without limitation,
+          improper network/server configurations, user error, and malicious
+          third-party behavior.
+        </p>
+        <p>
+          Let's Validate further does not represent or warrant that the SERVICE
+          will always function uninterrupted or error-free. Let's Validate makes
+          no warranty of limitations of service downtime or server availability.
+        </p>
+
+        <p>
+          No oral or written information or advice given by Let's Validate or by
+          any of its representatives, agents or resellers shall create a
+          warranty.
+        </p>
+
+        <p>
+          Because the SERVICE is offered free of charge, Let's Validate retains
+          the right to deny YOU service at any time for any reason. If YOU
+          request the service from the same IP address more frequently than
+          every five (5) minutes, YOUR request will be denied.
+        </p>
+
+        <p>
+          As some jurisdictions do not allow some of the exclusions or
+          limitations as set forth above, some of these exclusions or
+          limitations may not apply to YOU. If that is the case, the disclaimer
+          of warranties shall be issued TO THE MAXIMUM EXTENT PERMISSIBLE BY
+          APPLICABLE AND/OR EXISTING LAW.
+        </p>
+
+        <p><b>ARTICLE 6 LIMITATION OF LIABILITY</b></p>
+        <p>
+          In no event shall Let's Validate be liable for any indirect,
+          incidental, punitive, or consequential damages due to any possible
+          inefficacy or deficiency in the quality of the SERVICE.
+        </p>
+
+        <p>
+          In all other events, Let's Validate shall not be liable for any loss,
+          damage, injury (including, to the full extent permitted by law and no
+          further, personal injury or death), demand or expense, direct or
+          indirect, resulting from any act or omission that can be attributed to
+          Let's Validate, its directors, employees, agents, or any other cause
+          and arising out of or in connection with the use of the SERVICE even
+          if Let's Validate, its directors, employees, agents or resellers, have
+          been advised of the possibility of such damages, arising out of the
+          use, misuse, or inability to use the SERVICE.
+        </p>
+
+        <p>
+          While it may be foreseeable that an inability to properly keep precise
+          time may lead to consequential damages if incorporated into a computer
+          system, representations of the precision of the service are AS IS and
+          provided on a best effort basis, and any consequential and/or
+          incidental damage resulting from lack of precision time is TOTALLY
+          DISCLAIMED.
+        </p>
+
+        <p>
+          As some jurisdictions do not allow limitations of liability as set
+          forth above, some of these exclusions or limitations may not apply to
+          YOU. If that is the case, the disclaimer of warranties shall be issued
+          TO THE MAXIMUM EXTENT PERMISSIBLE BY APPLICABLE AND/OR EXISTING LAW.
+        </p>
+
+        <p><b>ARTICLE 7 THIRD PARTY SERVICE</b></p>
+        <p>
+          YOU acknowledge and agree that the SERVICE interacts with Third Party
+          Software, the operating system on its hosting servers, and the
+          Internet. Title and ownership of Third Party Software and the
+          intellectual property rights associated therewith are and remain the
+          sole and exclusively property of the respective third party. Let's
+          Validate makes no representations and warranties concerning the
+          efficacy and functionality of the operating system on its hosting
+          servers or any potential open source modifications made to YOUR
+          operating system
+        </p>
+
+        <p><b>ARTICLE 8 REPRESENTATIONS OF PRIVACY</b></p>
+        <p>
+          Let's Validate warrants that the consumer information that is
+          collected is limited to YOUR Name and Email Address. Furthermore,
+          Let's Validate warrants it does not: (a) collect or "screen-scrape"
+          any information found on your electronic device (b) collect or report
+          back to Let's Validate any data regarding YOUR computer keystrokes or
+          other data unrelated to the value the SERVICE provides. Let's Validate
+          further warrants it does not sell YOUR consumer proprietary
+          information, nor does Let's Validate distribute your consumer
+          proprietary information to any third parties for any purposes other
+          than to maximize the function and efficiency of SERVICE.
+        </p>
+
+        <p>
+          Let's Validate does keep Internet logs for the purpose of monitoring
+          the level of service provided. Such logs do contain Internet Protocol
+          (“IP”) address of its users. Let's Validate warrants that such user IP
+          information shall not be shared with any third-party unless required
+          by subpoena, judicial warrant or other legally authorized
+          administrative and/or judicial process, or on an aggregated basis
+          solely for the purpose of improving efficiency.
+        </p>
+        <p><b>ARTICLE 9 DONATIONS</b></p>
+        <p>
+          In cases where YOU may decide to donate money to Let's Validate, Let's
+          Validate is required to track donor identities, donor amounts, and
+          dates of donations in a manner required by all donors to any
+          non-profit entity designated by section 501(c)(3) of the United States
+          Internal Revenue Code. Let's Validate warrants that in such an event,
+          data collected is not sold or shared with any other party.
+        </p>
+
+        <p><b>ARTICLE 10 SEVERABILITY</b></p>
+        <p>
+          If any term or provision of this SERVICE shall be found to be illegal
+          or unenforceable, then, notwithstanding that term, all other terms of
+          this SERVICE shall remain in full force and effect and parties shall
+          substitute for the affected term or provision an enforceable term or
+          provision which approximates the intent and economic effect of the
+          affected provision.
+        </p>
+
+        <p><b>ARTICLE 11 WAIVER</b></p>
+        <p>
+          The failure by Let's Validate to exercise any right hereunder shall
+          not operate as a waiver of Let's Validate’s rights to exercise such
+          right or any other right in the future.
+        </p>
+
+        <p><b>ARTICLE 12 CHOICE OF LAW</b></p>
+        <p>
+          The laws of the Commonwealth of Virginia shall govern this SERVICE in
+          all respects.
+        </p>
+
+        <p><b>ARTICLE 13 MODIFICATIONS</b></p>
+        <p>
+          Let's Validate may, in its sole discretion, modify or replace these
+          Terms and Conditions at any time. YOUR continued use of the Services
+          following a change to these Terms and Conditions shall constitute
+          acceptance of such changes.
+        </p>
+
+        <p><b>ARTICLE 14 TOTAL INTEGRATION</b></p>
+        <p>
+          These Terms and Conditions represent the total sum of agreement
+          between the parties with respect to use of the SERVICE, and any
+          modifications of these Terms and Conditions shall only occur by mutual
+          subsequent written agreement between YOU and Let's Validate.
+        </p>
+
+        <p>
+          By using this SERVICE, you agree to the Terms and Conditions specified
+          herein.
+        </p>
+      </div>
     </div>
-  </template>
+  </main>
+</template>
