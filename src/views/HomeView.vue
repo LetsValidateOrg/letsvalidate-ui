57fe3e6236f4d090a250e8710ceee073084c228c
Cody <cody@cdeskins.com>
2023-11-06 20:55:36 -0700

    fixed homepage a bit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,34 +3,24 @@
     <div class="row">
       <div class="col">
         <h1>Automated Website Certificate Validation</h1>
-      </div>
-    </div>
-    <div class="row my-2">
-      <div class="col-7">
+        <br/>
         <h3>Maintain trust between your business and customers</h3>
         <p>When a site certificate is invalid, users will be warned that the site is no longer safe to visit.</p>
         <p>Let's Validate is a free, cloud-based online service (SaaS) that provides automated, daily monitoring of website certificate validity.  Let's Validate alerts you to expired, soon-to-expire, and invalit site certificate.</p>
       </div>
-      <div class="col-5">
-        <img src="../assets/work-risk-free.svg">
-      </div>
     </div>
     <div class="row">
-      <div class="home-cta my-2">
-        [CTA GOES HERE]
+      <div class="home-cta my-3 py-4 bg-secondary">
+        <a href="/signin">
+          [CTA GOES HERE]
+        </a>
       </div>
     </div>
-    <div class="row">
-      <div class="col-5">
-        <img src="../assets/work-risk-free.svg">
-      </div>
-      <div class="col-7">
+    <div class="row my-2">
+      <div class="col-7 mt-4">
         <h3>Never get caught with an invalid website certificate again</h3>
         <p>Let's Validate alerts you to expired, soon-to-expire, and invalid site certificates.  Valid certificates show as [valid state].  Certificates that require attention show as [invalid state].</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-7">
+        <br/>
         <h3>Get Notified before your certificate expires</h3>
         <p>Let's Validate will warn you with increasing frequency as a certificate reaches certain milestones:</p>
         <ul>
@@ -40,11 +30,6 @@
       </div>
       <div class="col-5">
         <img src="../assets/work-risk-free.svg">
-      </div>
-    </div>
-    <div class="row">
-      <div class="home-cta my-2">
-        [CTA GOES HERE]
       </div>
     </div>
   </div>
